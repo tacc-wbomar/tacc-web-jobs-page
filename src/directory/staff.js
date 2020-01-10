4a8ebc9dfbbf7b1b374b3893349b16d74a044894
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-01-10 16:16:36 -0600

    WES-6: Add comments and polish

--- src/directory/staff.js
+++ src/directory/staff.js
@@ -1,21 +1,29 @@
+// Include dependencies
 const fs = require('fs');
 const remark = require('remark');
 const remarkHtml = require('remark-html')
 
+// Get and parse staff data
 const staffRaw = fs.readFileSync('_staff.json');
 const staff = JSON.parse(staffRaw);
 
-let data = {};
-for (var member in staff) {
-  data = staff[member];
+/** Convert biography from Markdown to HTML */
+function parseBiography() {
+  let data = {};
+  for (var member in staff) {
+    data = staff[member];
 
-  if (! data.biography) {
-    return;
+    if (! data.biography) {
+      return;
+    }
+
+    data.biography = remark()
+      .use(remarkHtml)
+      .processSync(data.biography).toString();
   }
-
-  data.biography = remark()
-    .use(remarkHtml)
-    .processSync(data.biography).toString();
 }
+
+// Manipulate data
+parseBiography();
 
 module.exports = staff;
